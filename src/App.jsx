6c292887e15f84a0b39d5bdbef01a77fa7a6a3e4
ailojay <pauledukpe@yahoo.com>
2023-07-12 14:16:48 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import GoogleMapComponent from './components/Googlemap';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to My Google Maps App</h1>
+      <GoogleMapComponent />
+    </div>
+  );
+};
+
+export default App;
